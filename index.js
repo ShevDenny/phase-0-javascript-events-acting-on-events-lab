3e96b5fe4c59a4983735d793e3b65105e1b1946b
Shevon Denny <sdenny904@gmail.com>
2021-05-11 17:46:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -9,23 +9,22 @@ function moveDodgerLeft() {
     }
   } 
 
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
 
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-    
-    if (left > 40) {
-        dodger.style.left = `${left +1}px`
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
     }
   }
 
   document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    
+    else if (e.key === "ArrowRight") {
       moveDodgerRight();
     }
   });
